e2e438110c335fbc1ee9617bff59d108a7d2c620
Sam3peoplelife <kamishartem1@gmail.com>
2024-06-09 19:44:20 +0200

    init

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "821652ca-a51b-476a-9e3a-bccd944bfa3f" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    if (e.response) {
+      return res.status(e.response.status).json(e.response.data);
+    } else {
+      return res.status(500).json({ message: "An unknown error occurred." });
+    }
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001");
+});
